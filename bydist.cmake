a64d4d49580d95ababf00d598fd54df0303f1ddd
David Callu <callu.david@gmail.com>
2017-11-04 20:32:17 +0100

    fix(bydist): update package and byd to build on linux

---
+++ bydist.cmake
@@ -0,0 +1,9 @@
+
+set(BYDIST_ROOT "${CMAKE_CURRENT_LIST_DIR}")
+
+get_filename_component(CMUT_ROOT "${BYDIST_ROOT}/tools/cmut" ABSOLUTE)
+get_filename_component(BYD_ROOT "${BYDIST_ROOT}/tools/byd" ABSOLUTE)
+
+include("${BYD_ROOT}/byd.cmake")
+
+byd__add_package_repositories("${BYDIST_ROOT}/packages")

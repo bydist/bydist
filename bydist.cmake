95ef5ce020ac132b7b48051819c69727a48e5e45
David Callu <callu.david@gmail.com>
2018-01-19 15:43:13 +0100

    feat(cmake): extract bydist__configure_and_build from bydist.cmake, and add bydist_gateway.cmake

--- bydist.cmake
+++ bydist.cmake
@@ -4,10 +4,6 @@ set(BYDIST_ROOT "${CMAKE_CURRENT_LIST_DIR}")
 get_filename_component(CMUT_ROOT "${BYDIST_ROOT}/tools/cmut" ABSOLUTE)
 get_filename_component(BYD_ROOT "${BYDIST_ROOT}/tools/byd" ABSOLUTE)
 
-
-include("${BYDIST_ROOT}/cmake/bydist__option__build_dependencies.cmake")
-include("${BYDIST_ROOT}/cmake/configure_and_build.cmake")
-
 include("${BYD_ROOT}/byd.cmake")
 
 byd__add_package_repositories("${BYDIST_ROOT}/packages")
